a199599133fb77e96b63eb73345b61989b1a7239
russelbcabusao06 <russel.cabusao.b@bulsu.edu.ph>
2022-07-15 18:42:16 +0800

    commit on deleted php files

--- dbh.condo-name.php
+++
@@ -1,9 +0,0 @@
-**This is a test for the "NAME OF CONDO" on customer home page**
-<?php
-    $dbServername = "localhost";
-    $dbUsername = "root";
-    $dbPassword = "";
-    $db_ebts = "db_ebts";
-
-    $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $db_ebts);
-?>
