54457693bbed28cd34d4650e93b0bd9de8f49caa
Karlleml1 <qkladeleon@tip.edu.ph>
2022-07-18 08:36:47 +0800

    bookings commit

--- booking.php
+++ booking.php
@@ -3,7 +3,7 @@ include("config.php");
 ?>
 <html>
 <head>
-    <title>For Checkout</title>
+    <title>Bookings</title>
     <link rel="icon" href="image/TRANSPARENT BG.png">
     <link rel="stylesheet" href="fontawesome-free-5.15.1-web/css/all.css" type="text/css" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
