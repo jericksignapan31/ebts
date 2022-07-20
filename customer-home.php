338d9fc849a29bc7480c65d477ca5252ece3afb2
russelbcabusao06 <russel.cabusao.b@bulsu.edu.ph>
2022-07-20 15:03:58 +0800

    commit on reservation section

--- customer-home.php
+++ customer-home.php
@@ -333,7 +333,7 @@ require_once("config.php");
       <h3 style='font-size:1.17em;overflow:auto;font-family:Moonglade;'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-people' viewBox='0 0 16 16'>
     <path d='M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0zM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816zM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275zM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4z'/>
   </svg> <?php echo $row['capacity'] ?> </h3>
-      <br><button class='btn' style='width:100%;background-color:#b39a88;'><a href='information.php?id=" <?php echo $row['id'] ?> 'style='color:white;letter-spacing: 0.3em;text-decoration: none;'><b>Check Availability</b></a></button>
+      <br><button class='btn' style='width:100%;background-color:#b39a88;'><a href='information.php?id=<?php echo $row['id'] ?> 'style='color:white;letter-spacing: 0.3em;text-decoration: none;'><b>Check Availability</b></a></button>
       </div>
       <?php
     }
